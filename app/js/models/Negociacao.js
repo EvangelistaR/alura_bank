7fb0c2c7fb26840166d7bcee0c5ce94527107d9d
Rafael Evangelista <rafael.evans@live.com>
2020-05-10 20:54:44 -0300

    First commit, intall typescript  and setup the tsconfig

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,20 @@
+class Negociacao {
+    // Usando o underline convenciona que essas propriedades não podem ser acessadas
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
