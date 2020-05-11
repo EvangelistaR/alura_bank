2713619e6c3541e42032300822fe05f360637e36
= <=>
2020-05-10 23:31:09 -0300

    Ajusting align

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,31 +1,31 @@
 class Negociacao{
 
-  // Definição das propriedades
-  // Apenas métodos da própria classe poderão alterar as propriedades
-  private _data; 
-  private _quantidade;
-  private _valor;
+    // Definição das propriedades
+    // Apenas métodos da própria classe poderão alterar as propriedades
+    private _data; 
+    private _quantidade;
+    private _valor;
 
-  // Usando o underline convenciona que essas propriedades não podem ser acessadas
-  constructor(data, quantidade, valor){
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
+    // Usando o underline convenciona que essas propriedades não podem ser acessadas
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
 
-  get data(){
-    return this._data;
-  }
+    get data(){
+        return this._data;
+    }
 
-  get quantidade(){
-    return this._quantidade;
-  }
+    get quantidade(){
+        return this._quantidade;
+    }
 
-  get valor(){
-    return this._valor;
-  }
+    get valor(){
+        return this._valor;
+    }
 
-  get volume(){
-    return this._quantidade * this._valor;
-  }
+    get volume(){
+        return this._quantidade * this._valor;
+    }
 }
